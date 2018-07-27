17d78e02825ebcd70e8790eece7cd07da2cbf195
crkDan <39040993+crkDan@users.noreply.github.com>
2018-07-27 12:42:34 +0800

    crkJK
    abcd

---
+++ TheAvengersKingdom/Assets/Scripts/Json/People.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Json
+{
+    class People
+    {
+        public String Name { get; set;}
+        public int Id { get; set; }
+        public String Habit { get; set; }
+    }
+}
---
+++ TheAvengersKingdom/Assets/Scripts/Json/Test.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.Json;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        test();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void test() {
+        People ouput = new People();
+        ouput.Name = "劉政豪";
+        ouput.Id = 05130401;
+        ouput.Habit = "打球";
+
+        string output = JsonConvert.SerializeObject(ouput);
+        Debug.Log(output);
+
+        People input = JsonConvert.DeserializeObject<People>(output);
+        Debug.Log("ID : " + input.Id);
+        Debug.Log("Name : " + input.Name);
+        Debug.Log("Author : " + input.Habit);
+    }
+}
