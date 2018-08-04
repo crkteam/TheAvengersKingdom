7c19daafc9d66fac6b4c312abcd6a628322dbe26
crkts <39043206+crkts@users.noreply.github.com>
2018-08-04 17:26:40 +0800

    CrkTU

---
+++ Assets/Script/Button_Controll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Controll : MonoBehaviour {
+    public GameObject CharButton;
+    void Start()
+    {
+        CharButton.active = false;
+
+    }
+    public void ButtonChar()
+    {   //控制顯示和隱藏的方法
+        if (CharButton.active == false)
+        {
+            CharButton.SetActive(true);
+        }
+        else
+        {
+            CharButton.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Script/Injured.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Injured : MonoBehaviour {
+
+    //血量移动的速度
+    public float speed = 100;
+    // Use this for initialization
+    void Start()
+    {
+        // 物件消失時間!!
+        Destroy(gameObject, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //血量移动的效果
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+    }
+}
