73e35232860c4a01b708265dce57db397656945f
crkts <39043206+crkts@users.noreply.github.com>
2018-08-04 17:12:28 +0800

    CrkTU

---
+++ Assets/Resources/ABC.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ABC : ChangeFace {
+
+	// Use this for initialization
+	public void OnBtnHair()
+    {
+        
+        hair++;
+        if (hair > 3)
+            hair = 1;
+        Change("b", "hair", hair);
+        Debug.Log(hair);
+    }
+    public void OnBtnEye()
+    {
+        
+        eye--;
+        if (eye < 1)
+            eye = 3;
+        Change("a", "eye", eye);
+    }
+
+}
