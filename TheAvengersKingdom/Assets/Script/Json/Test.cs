0d48822961dea899e65b00a570cb05e567d20e80
crkts <39043206+crkts@users.noreply.github.com>
2018-09-04 21:31:35 +0800

    CrkTU
    !!!

--- TheAvengersKingdom/Assets/Script/Json/Test.cs
+++ TheAvengersKingdom/Assets/Script/Json/Test.cs
@@ -29,7 +29,7 @@ public class Test : MonoBehaviour {
         //cj.Save(Application.absoluteURL,"myPlayer");
 
 
-        TextMeshProUGUI level = GameObject.Find("Character_Level_Text").GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI level = GameObject.Find("Character_Level_Text ").GetComponent<TextMeshProUGUI>();
         level.text = "level:"+cj.character.level.ToString();
         TextMeshProUGUI hp = GameObject.Find("Character_HP_Text").GetComponent<TextMeshProUGUI>();
         hp.text = "hp:" + (cj.character.level * 20).ToString();
