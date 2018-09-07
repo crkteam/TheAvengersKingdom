adabe45a5c0492862b43b17ae6686f27f43f9495
littleg8 <qwerasdfzxcv112@gmail.com>
2018-09-07 19:00:26 +0800

    Crkgg
    SceneMove2.0

---
+++ TheAvengersKingdom/Assets/Script/Controll/SceneMove1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneMove1 : MonoBehaviour {
+
+	private GameObject background;
+	
+	public float speed;
+	private float mover;
+
+	private void Start()
+	{
+		Destroy(this,1f);
+	}
+
+	void Update()
+	{
+
+		background=GameObject.Find("Main_Background_Image");
+		mover = transform.position.x;
+		Debug.Log(mover);
+		transform.Translate(Vector3.left * Time.deltaTime * speed);
+		if (mover <= -1500)
+			background.GetComponent<Image>().color=Color.blue;
+
+	}
+}
