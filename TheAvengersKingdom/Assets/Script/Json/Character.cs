275b08e91a11920b0a4cac7ee5d717f191cbfd1f
crkDan <39040993+crkDan@users.noreply.github.com>
2018-09-10 16:08:23 +0800

    crk_dam
    9/10,4:07

---
+++ TheAvengersKingdom/Assets/Script/Json/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Json
+{
+    class Character
+    {
+        public String name;
+        public int level;
+        public int personality;
+        public int[] characteristic = new int[5];
+        public int[] characteristic_level = new int[5];
+        public int[] skill = new int[5];
+        public int[] skill_level = new int[5];
+    }
+}
