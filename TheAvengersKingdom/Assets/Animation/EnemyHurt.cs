617ea2cebe7d67669962bb3ad314cb89353342c3
crkDan <39040993+crkDan@users.noreply.github.com>
2018-09-10 13:17:50 +0800

    crk_dan
    9/10 1:17

---
+++ TheAvengersKingdom/Assets/Animation/EnemyHurt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnemyHurt : MonoBehaviour,IPointerDownHandler {
+    public Animator animator2;
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        animator2.SetTrigger("Hurt");
+    }
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+}
